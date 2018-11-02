eb047088eb8917a5b1b8423d7e29e8c2c221513a
bigskyfish <15939743797@163.com>
2018-11-02 23:25:29 +0800

    first commit by fuchb

---
+++ src/test/java/floatClould/floatClould/FloatClouldApplicationTests.java
@@ -0,0 +1,16 @@
+package floatClould.floatClould;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FloatClouldApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
